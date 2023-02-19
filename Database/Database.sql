8f525f18ab0c1cf73be0b727058e2dc0fe5cc7cd
aminhassal <aminhassal@yahoo.com>
2023-02-19 19:31:54 +0200

    1.8

--- Database/Database.sql
+++ Database/Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2023 at 10:11 PM
+-- Generation Time: Feb 19, 2023 at 06:30 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -44,6 +44,21 @@ INSERT INTO `admins` (`AdminID_PK`, `userName`, `Email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `attendence`
+-- (See below for the actual view)
+--
+CREATE TABLE `attendence` (
+`CourseiID` int(11)
+,`ClassID_FK` int(11)
+,`Name` varchar(30)
+,`RecordStatus` varchar(50)
+,`RecordTime` varchar(10)
+,`RecordDate` varchar(10)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `class`
 --
 
@@ -61,10 +76,11 @@ CREATE TABLE `class` (
 --
 
 INSERT INTO `class` (`ClassID`, `SubjectID_FK`, `OpenDate`, `IsActive`, `classname`, `LectureName`) VALUES
-(12, 1, '2023-02-14 00:00:00', b'1', 'رياضة 3', '555'),
+(12, 2, '2023-02-14 00:00:00', b'1', 'رياضة 1', '555'),
 (37, 2, '2023-02-16 00:00:00', b'1', '11', '15'),
 (39, 1, '2023-02-16 00:00:00', b'1', 'خريف انجليزي', 'أمين حصال'),
-(40, 2, '2023-02-16 00:00:00', b'1', 'رياضة', 'سهيلة');
+(40, 2, '2023-02-16 00:00:00', b'1', 'رياضة', 'سهيلة'),
+(41, 1, '2023-02-17 00:00:00', b'1', 'فصل الخريف', 'خليفة');
 
 -- --------------------------------------------------------
 
@@ -84,7 +100,14 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`CourseiID`, `ClassID_FK`, `CourseName`, `OpenDate`) VALUES
-(21, 12, 'المحاضرة الاولى', '2023-2-14');
+(21, 12, 'المحاضرة الاولى', '18-02-2023'),
+(35, 41, 'الاولى', '03-03-2023'),
+(36, 41, '', 'Array'),
+(37, 41, '', '2023-02-17'),
+(38, 41, '', '17-02-2023'),
+(39, 12, '', '17-02-2023'),
+(41, 12, '55', '19-02-2023'),
+(42, 12, 'الثانية', '19-02-2023');
 
 -- --------------------------------------------------------
 
@@ -119,7 +142,7 @@ CREATE TABLE `infostd` (
 --
 
 INSERT INTO `infostd` (`Uid`, `STD_id`, `Name`, `InRollNumber`, `Phone`, `Password`) VALUES
-(1, 1, 'حنان', 181911, 92891, 0),
+(1, 1, 'نزيهة', 181911, 92891, 0),
 (2, 2, 'amin', 111, 91, 0),
 (5, 5, 'عبدالسلام', 555, 5974, 0),
 (6, 6, 'هدى', 1553, 918915632, 0),
@@ -127,7 +150,8 @@ INSERT INTO `infostd` (`Uid`, `STD_id`, `Name`, `InRollNumber`, `Phone`, `Passwo
 (8, 8, 'كريم', 11233, 924951622, 0),
 (10, 10, 'هادي', 159357, 1584793, 0),
 (15, NULL, 'عمر', 1553, 924951622, 123456),
-(16, NULL, 'أمين', 12233, 928912363, 1233456);
+(16, NULL, 'أمين', 12233, 928912363, 1233456),
+(17, NULL, 'الياس', 100253, 92584635, 1233456);
 
 -- --------------------------------------------------------
 
@@ -138,9 +162,19 @@ INSERT INTO `infostd` (`Uid`, `STD_id`, `Name`, `InRollNumber`, `Phone`, `Passwo
 CREATE TABLE `masterofcourse` (
   `MasterOfCourseID_PK` int(11) NOT NULL,
   `StudentUID` int(11) NOT NULL,
-  `ClassID_FK` int(11) NOT NULL,
-  `RecordID_FK` int(11) NOT NULL
+  `RecordID_FK` int(11) NOT NULL,
+  `CourseiID_FK` int(11) NOT NULL,
+  `Attended` tinyint(1) DEFAULT NULL,
+  `ClassID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `masterofcourse`
+--
+
+INSERT INTO `masterofcourse` (`MasterOfCourseID_PK`, `StudentUID`, `RecordID_FK`, `CourseiID_FK`, `Attended`, `ClassID`) VALUES
+(118, 1, 55, 21, NULL, 12),
+(119, 15, 72, 21, NULL, 12);
 
 -- --------------------------------------------------------
 
@@ -161,15 +195,14 @@ CREATE TABLE `records` (
 --
 
 INSERT INTO `records` (`RecordID_PK`, `StudentUID`, `RecordStatus`, `RecordDate`, `RecordTime`) VALUES
-(47, 1, 'Check In', '23:02:16', '06-11-2022'),
+(47, 1, 'Check In', '02-02-2023', '06-11-2022'),
 (48, 1, 'Check In', '23:02:24', '06-11-2022'),
 (49, 1, 'Check In', '23:02:33', '06-11-2022'),
 (50, 1, 'Check In', '23:02:42', '06-11-2022'),
 (51, 1, 'Check In', '23:02:48', '06-11-2022'),
-(52, 1, 'Check In', '23:02:54', '06-11-2022'),
 (53, 1, 'Check In', '11:49:48', '08-11-2022'),
-(54, 2, 'Check In', '11:50:50', '08-11-2022'),
-(55, 1, 'Check In', '11:57:33', '08-11-2022'),
+(54, 2, 'Check In', '18-02-2023', '18-02-2023'),
+(55, 1, 'Check In', '18-02-2023', '18-02-2023'),
 (56, 1, 'Check In', '11:57:42', '08-11-2022'),
 (57, 2, 'Check In', '16:34:59', '08-11-2022'),
 (58, 2, 'Check In', '20:24:33', '10-11-2022'),
@@ -185,8 +218,8 @@ INSERT INTO `records` (`RecordID_PK`, `StudentUID`, `RecordStatus`, `RecordDate`
 (68, 2, 'Check In', '17:32:36', '08-12-2022'),
 (69, 1, 'Check In', '19:51:00', '11-12-2022'),
 (70, 1, 'Check In', '19:51:23', '11-12-2022'),
-(71, 1, 'Check In', '19:51:41', '11-12-2022'),
-(72, 1, 'Check In', '21:00:47', '12-12-2022'),
+(71, 1, 'Check In', '03-03-2023', '03-03-2023'),
+(72, 15, 'Check In', '18-02-2023', '18-02-2023'),
 (73, 2, 'Check In', '22:09:19', '31-01-2023'),
 (74, 2, 'Check In', '22:10:24', '31-01-2023'),
 (75, 1, 'Check In', '17:21:24', '01-02-2023'),
@@ -221,26 +254,25 @@ CREATE TABLE `studentclass` (
 --
 
 INSERT INTO `studentclass` (`StudentClassID`, `ClassID`, `StdUid_FK`) VALUES
-(1, 12, 2),
-(8, 12, 10),
-(9, 12, 10),
-(10, 12, 5),
-(11, 12, 8),
-(12, 12, 5),
-(13, 12, 10),
-(14, 37, 2);
+(30, 12, 1),
+(37, 12, 15),
+(36, 12, 17),
+(31, 39, 1),
+(32, 39, 2),
+(19, 40, 7),
+(35, 41, 17);
 
 -- --------------------------------------------------------
 
 --
--- Stand-in structure for view `studentinsubjects`
+-- Stand-in structure for view `students_record`
 -- (See below for the actual view)
 --
-CREATE TABLE `studentinsubjects` (
-`ClassID` int(11)
-,`Name` varchar(30)
-,`InRollNumber` int(15)
+CREATE TABLE `students_record` (
+`RecordID_PK` int(11)
+,`ClassID` int(11)
 ,`StdUid_FK` int(11)
+,`CourseiID` int(11)
 );
 
 -- --------------------------------------------------------
@@ -254,6 +286,7 @@ CREATE TABLE `student_in_class` (
 ,`Name` varchar(30)
 ,`InRollNumber` int(15)
 ,`ClassID` int(11)
+,`StudentClassID` int(11)
 );
 
 -- --------------------------------------------------------
@@ -279,11 +312,20 @@ INSERT INTO `subjects` (`SubjectID_PK`, `SubjectName`, `SubjectCode`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure for view `studentinsubjects`
+-- Structure for view `attendence`
 --
-DROP TABLE IF EXISTS `studentinsubjects`;
+DROP TABLE IF EXISTS `attendence`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `studentinsubjects`  AS SELECT `studentclass`.`ClassID` AS `ClassID`, `infostd`.`Name` AS `Name`, `infostd`.`InRollNumber` AS `InRollNumber`, `studentclass`.`StdUid_FK` AS `StdUid_FK` FROM ((`infostd` join `studentclass` on(`infostd`.`Uid` = `studentclass`.`StdUid_FK`)) join `class` on(`studentclass`.`ClassID` = `class`.`ClassID`))  ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `attendence`  AS SELECT `courses`.`CourseiID` AS `CourseiID`, `courses`.`ClassID_FK` AS `ClassID_FK`, `infostd`.`Name` AS `Name`, `records`.`RecordStatus` AS `RecordStatus`, `records`.`RecordTime` AS `RecordTime`, `records`.`RecordDate` AS `RecordDate` FROM (((`masterofcourse` join `infostd` on(`masterofcourse`.`StudentUID` = `infostd`.`Uid`)) join `records` on(`records`.`RecordID_PK` = `masterofcourse`.`RecordID_FK`)) left join `courses` on(`masterofcourse`.`CourseiID_FK` = `courses`.`CourseiID`))  ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `students_record`
+--
+DROP TABLE IF EXISTS `students_record`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `students_record`  AS SELECT `records`.`RecordID_PK` AS `RecordID_PK`, `studentclass`.`ClassID` AS `ClassID`, `studentclass`.`StdUid_FK` AS `StdUid_FK`, `courses`.`CourseiID` AS `CourseiID` FROM ((`studentclass` join `records` on(`studentclass`.`StdUid_FK` = `records`.`StudentUID`)) join `courses` on(`studentclass`.`ClassID` = `courses`.`ClassID_FK` and `records`.`RecordDate` = `courses`.`OpenDate`))  ;
 
 -- --------------------------------------------------------
 
@@ -292,7 +334,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `student_in_class`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_in_class`  AS SELECT `infostd`.`Uid` AS `Uid`, `infostd`.`Name` AS `Name`, `infostd`.`InRollNumber` AS `InRollNumber`, `studentclass`.`ClassID` AS `ClassID` FROM (((`class` join `subjects` on(`class`.`SubjectID_FK` = `subjects`.`SubjectID_PK`)) join `studentclass` on(`class`.`ClassID` = `studentclass`.`ClassID`)) join `infostd` on(`studentclass`.`StdUid_FK` = `infostd`.`Uid`))  ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_in_class`  AS SELECT `infostd`.`Uid` AS `Uid`, `infostd`.`Name` AS `Name`, `infostd`.`InRollNumber` AS `InRollNumber`, `studentclass`.`ClassID` AS `ClassID`, `studentclass`.`StudentClassID` AS `StudentClassID` FROM (((`class` join `subjects` on(`class`.`SubjectID_FK` = `subjects`.`SubjectID_PK`)) join `studentclass` on(`class`.`ClassID` = `studentclass`.`ClassID`)) join `infostd` on(`studentclass`.`StdUid_FK` = `infostd`.`Uid`))  ;
 
 --
 -- Indexes for dumped tables
@@ -335,9 +377,10 @@ ALTER TABLE `infostd`
 --
 ALTER TABLE `masterofcourse`
   ADD PRIMARY KEY (`MasterOfCourseID_PK`),
-  ADD KEY `B` (`ClassID_FK`),
+  ADD UNIQUE KEY `StudentUID_2` (`StudentUID`,`CourseiID_FK`),
   ADD KEY `StudentUID` (`StudentUID`),
-  ADD KEY `RecordID_FK` (`RecordID_FK`);
+  ADD KEY `RecordID_FK` (`RecordID_FK`),
+  ADD KEY `CourseiID_FK` (`CourseiID_FK`);
 
 --
 -- Indexes for table `records`
@@ -351,8 +394,8 @@ ALTER TABLE `records`
 --
 ALTER TABLE `studentclass`
   ADD PRIMARY KEY (`StudentClassID`),
-  ADD KEY `STDclassStudents` (`StdUid_FK`),
-  ADD KEY `STDclassClasses` (`ClassID`);
+  ADD UNIQUE KEY `ClassID` (`ClassID`,`StdUid_FK`),
+  ADD KEY `STDclassStudents` (`StdUid_FK`);
 
 --
 -- Indexes for table `subjects`
@@ -374,13 +417,13 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `ClassID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `ClassID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `CourseiID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `CourseiID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `devices`
@@ -392,13 +435,13 @@ ALTER TABLE `devices`
 -- AUTO_INCREMENT for table `infostd`
 --
 ALTER TABLE `infostd`
-  MODIFY `Uid` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `Uid` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `masterofcourse`
 --
 ALTER TABLE `masterofcourse`
-  MODIFY `MasterOfCourseID_PK` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MasterOfCourseID_PK` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 
 --
 -- AUTO_INCREMENT for table `records`
@@ -410,7 +453,7 @@ ALTER TABLE `records`
 -- AUTO_INCREMENT for table `studentclass`
 --
 ALTER TABLE `studentclass`
-  MODIFY `StudentClassID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `StudentClassID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `subjects`
@@ -438,9 +481,9 @@ ALTER TABLE `courses`
 -- Constraints for table `masterofcourse`
 --
 ALTER TABLE `masterofcourse`
-  ADD CONSTRAINT `CLASSTOMASTER` FOREIGN KEY (`ClassID_FK`) REFERENCES `class` (`ClassID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Course_To_master` FOREIGN KEY (`CourseiID_FK`) REFERENCES `courses` (`CourseiID`),
   ADD CONSTRAINT `RecordToMasterOfCourse` FOREIGN KEY (`RecordID_FK`) REFERENCES `records` (`RecordID_PK`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Std_To_Master` FOREIGN KEY (`StudentUID`) REFERENCES `infostd` (`Uid`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `Std_To_Master` FOREIGN KEY (`StudentUID`) REFERENCES `infostd` (`Uid`);
 
 --
 -- Constraints for table `records`
